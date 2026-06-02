183e5c3bd62d431867a6f2ba8ba32cf8670c3f59
monn <pphan@paragoniu.edu.kh>
2026-06-02 17:21:19 +0700

    help page clickable

--- frontend/src/components/taskbar/NavBar.vue
+++ frontend/src/components/taskbar/NavBar.vue
@@ -1,6 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import SignIcon from '../../assets/SignIcon.vue'
 import QuestionIcon from '../../assets/QuestionIcon.vue'
+import Help from '../Help.vue'
+
+const showHelp = ref(false)
 </script>
 
 <template>
@@ -9,16 +13,17 @@ import QuestionIcon from '../../assets/QuestionIcon.vue'
             <div class="w-10 flex items-center justify-center">
                 <SignIcon />
             </div>
+
             <h1 class="text-xl font-bold text-black">
                 CBDSL <span class="text-blue-900">Recognition</span>
             </h1>
         </div>
 
-        <div class="flex items-center gap-2 text-black-300 text-sm">
-            <div >
-                <QuestionIcon />
-            </div>
-            <a href="#" class="hover:text-blue-900">Help</a>
-        </div>
+        <button @click="showHelp = true" class="flex items-center gap-2 text-sm text-gray-500 cursor-pointer hover:text-blue-900">
+            <QuestionIcon />
+            Help
+        </button>
+
+        <Help :show="showHelp" @close="showHelp = false" />
     </nav>
 </template>
